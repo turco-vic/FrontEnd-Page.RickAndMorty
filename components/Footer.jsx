3bc6cdc80d1184e1e1837880966a4c7ff1e54d61
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-04-10 21:19:50 -0300

    Update Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <div className={styles.container}>
             <p className={styles.copy}>2025 Rick and Morty. Todos os direitos reservados.</p>
+            <p className={styles.author}>Enzo Turcovic</p>
         </div>
     )
 }
